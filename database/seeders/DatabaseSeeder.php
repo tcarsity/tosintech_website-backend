26d5e84ecb105874e46f37a563fed8010ea459db
unknown <you@example.com>
2026-01-08 10:49:08 +0100

    db seed added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::updateOrCreate(
+            ['email' => 'tosinoluwaseun10@gmail.com'],
+            [
+                'name' => 'Tosin Olarewaju',
+                'password' => Hash::make('tosintech91'),
 
-        User::factory()->create([
-            'name' => 'Tosin Olarewaju',
-            'email' => 'tosinoluwaseun10@gmail.com',
-        ]);
+            ]
+        );
     }
 }
