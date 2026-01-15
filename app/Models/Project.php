0ffa131205659553fbc28044e9a4beebf03beee4
unknown <you@example.com>
2026-01-15 22:21:18 +0100

    added model fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+    protected $fillable = [
+        'title',
+        'content',
+        'site',
+        'status'
+    ];
 }
